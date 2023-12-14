6bf7e3fac5168e0e02fcac8ce58b3b1917630549
PANKAJ KUMAR SHARMA <63609112+pankajk156@users.noreply.github.com>
2023-12-14 15:56:08 +0000

    datatypes

--- 01_basics/02_data_Types.js
+++ 01_basics/02_data_Types.js
@@ -1,3 +1,31 @@
 "use strict"; // treat all JS code as newer version
 
-alert("Hello")
+// alert("Hello") // we are using nodejs, not broser
+
+// console.log(3+3); console.log("Pankaj") It decrease the code readability.
+ 
+let name = "pankaj"
+let age = 18
+let isLoggedIn=false
+let state = null //in this case the value is null not undefined.
+
+
+// - Limit - Number -> 2 to power 53
+// bigint - using for high numbers
+// string - ""
+// boolean - either true or false
+// null - it is a standalone value also datatype
+// undefined - > if value not defined.
+// symbol - > use for unique 
+
+// -^ premetive data types above all.
+
+
+// object
+
+console.log(typeof(state));
+
+console.log(typeof  undefined);
+console.log(typeof null);
+
+
